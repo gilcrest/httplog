8d7e65ed8f659e7cca3d84b82fab4a4f99f1cb46
Dan Gillis <gilcrest@gmail.com>
2018-09-05 22:57:32 -0400

    Numerous changes mostly around context

--- middleware.go
+++ middleware.go
@@ -8,46 +8,49 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// HTTPLog records and logs as much as possible about an
-// incoming HTTP request and response
-func HTTPLog(log zerolog.Logger, db *sql.DB, opts *Opts) Adapter {
+// LogAdapter records and logs as much as possible about an
+// incoming HTTP request and response using the Adapter pattern
+func LogAdapter(log zerolog.Logger, db *sql.DB, o *Opts) Adapter {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 
 			var (
-				o   *Opts
-				err error
+				opts *Opts
+				err  error
 			)
 
-			if opts == nil {
-				o, err = newHTTPLogOpts()
+			if o == nil {
+				opts, err = newHTTPLogOpts()
 				if err != nil {
 					log.Error().Err(err).Msg("")
 					return
 				}
 			} else {
-				o = opts
+				opts = o
 			}
 
-			aud := new(APIAudit)
-
+			// Pull the context from the request
 			ctx := req.Context()
 
-			startTimer(aud)
-
-			ctx = SetRequestID(ctx)
-
-			err = logReqDispatch(ctx, log, aud, req, o)
+			// Create an instance of APIaudit and pass it to startTimer
+			// to begin the API response timer
+			ctx, aud, err := newAPIAudit(ctx, log, req)
 			if err != nil {
 				http.Error(w, "Unable to log request", http.StatusBadRequest)
 				return
 			}
+			aud.startTimer()
 
-			ctx = SetRequestHost(ctx, aud)
-			ctx = SetRequestPort(ctx, aud)
-			ctx = SetRequestPath(ctx, aud)
-			ctx = SetRequestRawQuery(ctx, aud)
-			ctx = SetRequestFragment(ctx, aud)
+			ctx = setRequest2Context(ctx, aud)
+
+			// RequestLogController determines which of the logging methods
+			// you wish to use will be employed (based on the options passed in)
+			// It will also populate the APIAudit struct based on the incoming request
+			err = requestLogController(ctx, log, aud, req, opts)
+			if err != nil {
+				http.Error(w, "Unable to log request", http.StatusBadRequest)
+				return
+			}
 
 			rec := httptest.NewRecorder()
 			h.ServeHTTP(rec, req.WithContext(ctx))
@@ -64,24 +67,101 @@ func HTTPLog(log zerolog.Logger, db *sql.DB, opts *Opts) Adapter {
 			b := rec.Body.Bytes()
 			w.Write(b)
 
-			stopTimer(aud)
+			aud.stopTimer()
 
 			// write the data back to the recorder buffer as
 			// it's needed for SetResponse
 			rec.Body.Write(b)
 
 			// set the response data in the APIAudit object
-			err = setResponse(log, aud, rec)
+			err = aud.setResponse(log, rec)
 			if err != nil {
 				http.Error(w, "Unable to set response", http.StatusBadRequest)
 			}
 
-			// call logRespDispatch to determine if and where to log
-			err = logRespDispatch(ctx, log, db, aud, o)
+			// call responseLogController to determine if and where to log
+			err = responseLogController(ctx, log, db, aud, opts)
 			if err != nil {
-				http.Error(w, "Error from response dispatch", http.StatusBadRequest)
+				http.Error(w, "Error from responseLogController", http.StatusBadRequest)
 			}
-
 		})
+	}
+}
+
+// Log records and logs as much as possible about an
+// incoming HTTP request and response
+func Log(h http.Handler, log zerolog.Logger, db *sql.DB, o *Opts) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		var (
+			opts *Opts
+			err  error
+		)
+
+		if o == nil {
+			opts, err = newHTTPLogOpts()
+			if err != nil {
+				log.Error().Err(err).Msg("")
+				return
+			}
+		} else {
+			opts = o
+		}
+
+		// Pull the context from the request
+		ctx := req.Context()
+
+		// Create an instance of APIaudit and pass it to startTimer
+		// to begin the API response timer
+		ctx, aud, err := newAPIAudit(ctx, log, req)
+		if err != nil {
+			http.Error(w, "Unable to log request", http.StatusBadRequest)
+			return
+		}
+
+		aud.startTimer()
+
+		ctx = setRequest2Context(ctx, aud)
+
+		// RequestLogController determines which of the logging methods
+		// you wish to use will be employed (based on the options passed in)
+		err = requestLogController(ctx, log, aud, req, opts)
+		if err != nil {
+			http.Error(w, "Unable to log request", http.StatusBadRequest)
+			return
+		}
+
+		rec := httptest.NewRecorder()
+		h.ServeHTTP(rec, req.WithContext(ctx))
+
+		// copy everything from response recorder
+		// to actual response writer
+		for k, v := range rec.HeaderMap {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(rec.Code)
+
+		// pull out the response body and write it
+		// back to the response writer
+		b := rec.Body.Bytes()
+		w.Write(b)
+
+		aud.stopTimer()
+
+		// write the data back to the recorder buffer as
+		// it's needed for SetResponse
+		rec.Body.Write(b)
+
+		// set the response data in the APIAudit object
+		err = aud.setResponse(log, rec)
+		if err != nil {
+			http.Error(w, "Unable to set response", http.StatusBadRequest)
+		}
+
+		// call responseLogController to determine if and where to log
+		err = responseLogController(ctx, log, db, aud, opts)
+		if err != nil {
+			http.Error(w, "Error from responseLogController", http.StatusBadRequest)
+		}
 	}
 }
